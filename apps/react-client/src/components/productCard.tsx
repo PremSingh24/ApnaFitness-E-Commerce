9c9e5f1b2c74392a5705436cf81109c91af2878d
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-15 22:26:53 +0530

    refactor: removed unnecessary code

--- apps/react-client/src/components/productCard.tsx
+++ apps/react-client/src/components/productCard.tsx
@@ -2,7 +2,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, Container, CssBaseline, Grid, IconButton, Rating } from '@mui/material';
+import { Button, CardActionArea, CardActions, Container, CssBaseline, Divider, Grid, IconButton, Rating } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Loader from './loader';
 import { NavLink } from 'react-router-dom';
@@ -24,6 +24,7 @@ const theme = createTheme();
 
 
 const ProductCard = ({products}:{products:productType[]}) => {
+    
     const loggedIn = useLoginStore((state)=>state.login) || localStorage.getItem("loggedIn") ;
     const setLogOut = useLoginStore((state)=>state.setLogOut)
     const navigate = useNavigate();
@@ -103,9 +104,11 @@ const ProductCard = ({products}:{products:productType[]}) => {
             return JSON.stringify(product) === JSON.stringify(item);
         });
         
-        if(contains){
+        
             
-            return (
+        return (
+            <>
+            {contains ? 
                 <IconButton
                     size="large"
                     aria-label="show wishlist"
@@ -115,10 +118,7 @@ const ProductCard = ({products}:{products:productType[]}) => {
                 >
                     <FavoriteIcon color='error' />
                 </IconButton>
-            )
-        }else{
-            
-            return (
+                :
                 <IconButton
                     size="large"
                     aria-label="show wishlist"
@@ -128,38 +128,42 @@ const ProductCard = ({products}:{products:productType[]}) => {
                 >
                     <FavoriteBorderIcon />
                 </IconButton>
-            )
-        }
+            }
+            </>
+                
+        )
+        
     }
 
     const cartButton = (product:productType) => {
-        if(product.inStock){
-            if(cart.filter(obj=>obj.item._id ===product._id).length>0){
-                return (
-                    <Button variant='outlined' size="medium" 
+        return (
+            <>
+        {product.inStock ?
+
+            (cart.filter((obj)=>obj.item._id ===product._id).length>0 ?
+                <Button variant='outlined' size="medium" 
                     fullWidth 
                     onClick={()=> loggedIn ? navigate("/MyCart"):navigate("/login")}>
                         Go To Cart
-                    </Button>
-                )
+                </Button>
+            :
+                
 
-            }else{
-                return (
-
-                    <Button variant='contained' size="medium" 
+                <Button variant='contained' size="medium" 
                     fullWidth 
                     onClick={()=> loggedIn ? addToCart(product,product._id):navigate("/login")}>
                         Add To Cart
-                    </Button>
-                )
-            }
-
-        }else{
-            return (
-                <Button variant='contained' size="medium" color='error' fullWidth disabled>Out Of Stock</Button>
+                </Button>
+                
             )
 
-        } 
+        
+            :
+            <Button variant='contained' size="medium" color='error' fullWidth disabled>Out Of Stock</Button>
+        }
+        
+        </>
+        ) 
     }
 
 
@@ -170,77 +174,81 @@ const ProductCard = ({products}:{products:productType[]}) => {
     <Container maxWidth="xl" sx={{position:"relative",paddingLeft:0,paddingRight:0,}}>
         
     {products.length > 0 ?
-    <Grid container spacing={2} sx={{marginTop:"20px",}}>
-        {products.map((product:productType) =>   
+        <Grid container spacing={2} sx={{marginTop:"20px",}}>
+            {products.map((product:productType) =>   
 
-            <Grid item xs={6} sm={6} md={4} lg={3} xl={3} key={product._id} sx={{marginLeft:-0.6,marginRight:0.5}}>
-            <Card sx={{height:{xs:390,sm:470,md:460}, width: {xs:183,sm:215,md:230,lg:290},
-                padding:{xs:"0px",sm:"10px"},marginBottom:"20px", position:"relative", ":hover":{boxShadow:"20"},}} 
-            >
-            <NavLink
-                to={`/products/${product._id}`}
-                style={{ textDecoration: 'none' }}
-                
-            >
-                <CardActionArea >
-                    <CardMedia
-                    component="img"
-                    
-                    image={product.image}
-                    alt={product.title}
-                    sx={{objectFit:"contain",aspectRatio:"3/2",height:{xs:150,sm:200}}}
-                    />
-                    
-                    <CardContent>
-                    <Typography  color={"black"} sx={{fontSize:{xs:"1.2rem",},fontWeight:"bold",}}>
-                        {product.title}
-                    </Typography>
-                    <Typography gutterBottom color={"text.secondary"} 
-                        sx={{fontSize:{xs:"0.8rem",md:"1rem"},fontWeight:{xs:"bold",md:"normal"}}}
-                    >
-                        {product.description}
-                    </Typography>
-                    <Typography  color="text.primary" paddingRight={1} sx={{fontWeight:"bold",fontSize:"1.2rem"}}>
-                        ₹{product.currentPrice}   
-                    </Typography>
-                    <div style={{display:"flex", justifyContent:"flex-start"}}>
-                        <Typography variant="body2" color="text.secondary" paddingRight={1} 
-                            sx={{textDecoration:"line-through",fontSize:"1rem"}}
-                        >
-                            ₹{product.initialPrice} 
-                        </Typography>
-                        <Typography variant="body2" color="green" sx={{fontWeight:"bold",fontSize:"0.9rem"}}>
-                            {Math.round(100-((product.currentPrice/product.initialPrice)*100))}%OFF   
-                        </Typography>
-                    </div>
-                    <Rating name="read-only" value={product.rating} readOnly />
-                    </CardContent>
-                </CardActionArea>
-                </NavLink>
-                <CardActions sx={{position:"absolute",bottom:0,width:"90%",paddingRight:0,left:{xs:5,md:"auto"}}}>
-                    {cartButton(product)}
-                </CardActions>
-                {wishlistButton(product)}
-                {product.isDeliveredFast ?
-                <IconButton
-                    size="large"
-                    aria-label="fast delivery"
-                    color="error"
-                    sx={{position:"absolute",top:0,left:0}}
-                    disabled
+                <Grid item xs={6} sm={6} md={4} lg={3} xl={3} key={product._id} sx={{marginLeft:-0.6,marginRight:0.5}}>
+                <Card sx={{height:{xs:390,sm:470,md:460}, width: {xs:183,sm:215,md:230,lg:290},
+                    padding:{xs:"0px",sm:"10px"},marginBottom:"20px", position:"relative", ":hover":{boxShadow:"20"},}} 
                 >
-                    <Typography variant='body2' color={"white"} sx={{backgroundColor:"red"}}>Fast Delivery</Typography>
-                </IconButton>
-                :null
-                }
-                
-            </Card>
-            </Grid>
-        )}
-        
-    </Grid>
-    : <Loader/>
-                }
+                <NavLink
+                    to={`/products/${product._id}`}
+                    style={{ textDecoration: 'none' }}
+                    
+                >
+                    <CardActionArea >
+                        <CardMedia
+                        component="img"
+                        
+                        image={product.image}
+                        alt={product.title}
+                        sx={{objectFit:"contain",aspectRatio:"3/2",height:{xs:150,sm:200}}}
+                        />
+                        <Divider sx={{width:"100vw",position:"absolute",left:-10,right:0}}/>
+                        
+                        
+                        <CardContent>
+                        
+                        <Typography  color={"black"} sx={{fontSize:{xs:"1rem",md:"1.2rem",},fontWeight:"bold",}}>
+                            {product.title}
+                        </Typography>
+                        <Typography gutterBottom color={"text.secondary"} 
+                            sx={{fontSize:{xs:"0.8rem",md:"1rem"},fontWeight:{xs:"bold",md:"normal"}}}
+                        >
+                            {product.description}
+                        </Typography>
+                        <Typography  color="text.primary" paddingRight={1} sx={{fontWeight:"bold",fontSize:"1.2rem"}}>
+                            ₹{product.currentPrice}   
+                        </Typography>
+                        <div style={{display:"flex", justifyContent:"flex-start"}}>
+                            <Typography variant="body2" color="text.secondary" paddingRight={1} 
+                                sx={{textDecoration:"line-through",fontSize:"1rem"}}
+                            >
+                                ₹{product.initialPrice} 
+                            </Typography>
+                            <Typography variant="body2" color="green" sx={{fontWeight:"bold",fontSize:"0.9rem"}}>
+                                {Math.round(100-((product.currentPrice/product.initialPrice)*100))}%OFF   
+                            </Typography>
+                        </div>
+                        <Rating name="read-only" value={product.rating} readOnly />
+                        </CardContent>
+                    </CardActionArea>
+                    </NavLink>
+                    <CardActions sx={{position:"absolute",bottom:0,width:"90%",paddingRight:0,left:{xs:5,md:"auto"}}}>
+                        {cartButton(product)}
+                    </CardActions>
+                    {wishlistButton(product)}
+                    {product.isDeliveredFast ?
+                    <IconButton
+                        size="large"
+                        aria-label="fast delivery"
+                        color="error"
+                        sx={{position:"absolute",top:0,left:0}}
+                        disabled
+                    >
+                        <Typography variant='body2' color={"white"} sx={{backgroundColor:"red"}}>Fast Delivery</Typography>
+                    </IconButton>
+                    :null
+                    }
+                    
+                </Card>
+                </Grid>
+            )}
+            
+        </Grid>
+    : 
+        <Loader/>
+    }
     </Container>
     </ThemeProvider>
   );
