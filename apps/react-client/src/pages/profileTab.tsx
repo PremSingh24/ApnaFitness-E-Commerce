96502de7a70d869296d49782d6bb5ade801dab44
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-17 23:41:19 +0530

    style: Improved styling and layout for user profile tab

--- apps/react-client/src/pages/profileTab.tsx
+++ apps/react-client/src/pages/profileTab.tsx
@@ -1,9 +1,48 @@
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import { Divider } from '@mui/material';
+import { Box, Card, CardContent, styled } from '@mui/material';
 import useUsertStore from '../contexts/user.context';
 import LogOutButton from '../components/logOutButton';
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+
+
+const StyledCard = styled(Card)(({ theme }) => ({
+    width: '350px',
+    [theme.breakpoints.up('sm')]: {
+      width: '480px', 
+    },
+    margin: 'auto',
+    marginTop:"4rem",
+    textAlign: 'center',
+    borderRadius: theme.spacing(2),
+    boxShadow: '0px 8px 20px rgba(0, 0, 0, 0.1)',
+    backgroundColor: theme.palette.background.paper,
+    border: `1px solid ${theme.palette.primary.main}`,
+    
+}));
+
+const StyledCardContent = styled(CardContent)(({ theme }) => ({
+  paddingBottom: `${theme.spacing(2)} !important`,
+  padding: theme.spacing(3),
+}));
+
+const ProfileItem = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginBottom: theme.spacing(2),
+  
+  
+}));
+
+const IconWrapper = styled('span')(({ theme }) => ({
+  marginRight: theme.spacing(1),
+  color: theme.palette.primary.main,
+  display:"flex",
+  
+  
+}));
 
 
 
@@ -12,109 +51,51 @@ const ProfileTab = () =>{
 
     return (
         <>
-        <Paper elevation={5}
-          sx={{
-            p: 4,
-            margin: 'auto',
-            maxWidth: 400,
-            flexGrow: 1,
-            marginTop:"5rem",
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-              
-          }}
-        >
-          <Grid container spacing={2} >
-            <Grid item  xs={12} md={12}>
-                <Typography variant='h5' color={"black"} 
-                sx={{display:"flex",justifyContent:"center",
-                alignItems:"center",border:"2px solid black"}}
-                >
-                    My Profile
-                </Typography>
-            </Grid>
-    
-            <Grid item sx={{display:"flex",justifyContent:"start",alignItems:"flex-start"}} xs={6} md={6}>
-                <Typography variant='h6' color={"black"} 
-                sx={{display:"flex",justifyContent:"start",
-                alignItems:"start"}}
-                >Name</Typography>
-                
-            </Grid>
-
-            <Grid item sx={{display:"flex",justifyContent:"end",alignItems:"flex-end"}} xs={6} md={6}>
-                <Typography variant='h6' color={"black"} 
-                    sx={{display:"flex",justifyContent:"end",
-                    alignItems:"flex-end"}}
-                    >
-                        {user.firstName} {user.lastName}
-                </Typography>
-    
-            </Grid>
+        
+        <StyledCard>
+        <StyledCardContent>
+            <PersonIcon sx={{ fontSize: 60, marginBottom: 1 }} />
+            <Typography variant="h5" gutterBottom>
+            User Profile
+            </Typography>
             
-            
-            <Grid item sx={{display:"flex",justifyContent:"start",alignItems:"flex-start"}} xs={6} md={6}>
-                <Typography variant='h6' color={"black"} 
-                    sx={{display:"flex",justifyContent:"start",
-                    alignItems:"start"}}
-                    >
-                        Mobile
+            <ProfileItem>
+                <IconWrapper>
+                    <PersonIcon />
+                </IconWrapper>
+                <Typography variant="h6">
+                    {user.firstName} {user.lastName}
                 </Typography>
-            </Grid>
+            </ProfileItem>
 
-            <Grid item sx={{display:"flex",justifyContent:"end",alignItems:"flex-end"}} xs={6} md={6}>
-    
-                <Typography variant='h6' color={"black"} 
-                sx={{display:"flex",justifyContent:"end",
-                alignItems:"flex-end"}}
-                >
+            <ProfileItem>
+                <IconWrapper>
+                    <PhoneIcon />
+                </IconWrapper>
+                <Typography variant="h6">
                     {user.mobile}
                 </Typography>
-                
-            </Grid>
+            </ProfileItem>
 
             {user.email !== "" ?
-            <>
-
-            <Grid item sx={{display:"flex",justifyContent:"start",alignItems:"flex-start"}} xs={6} md={6}>
-                <Typography variant='h6' color={"black"} 
-                    sx={{display:"flex",justifyContent:"start",
-                    alignItems:"start"}}
-                >
-                    Email
-                </Typography>
-            </Grid>
             
-            <Grid item sx={{display:"flex",justifyContent:"end",alignItems:"flex-end"}} xs={6} md={6}>
-    
-                <Typography variant='h6' color={"black"} 
-                sx={{display:"flex",justifyContent:"end",
-                alignItems:"flex-end"}}
-                >
-                   {user.email}
+            <ProfileItem>
+                <IconWrapper>
+                    <EmailIcon />
+                </IconWrapper>
+                <Typography variant="h6">
+                    {user.email}
                 </Typography>
-                
-            </Grid>
-            </>
+            </ProfileItem>
+
             :
-            null
+                null
             }
-            
-            <Divider sx={{width:"100%"}} />
-
-            <Grid item sx={{display:"flex",justifyContent:"center",alignItems:"center"}} xs={12} md={12}>
             <LogOutButton/>
+        </StyledCardContent>
+    </StyledCard>
+    </>
 
-            </Grid>
-    
-            
-          </Grid>
-          
-        </Paper>
-
-        </>
-        
-        
     )
 }
 
