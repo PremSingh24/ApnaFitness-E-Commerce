af5d9bdc929d8ec043aecbf7d8beb055693e3e8c
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-17 20:07:45 +0530

    refactor: improved card responsiveness

--- apps/react-client/src/components/cartProduct.tsx
+++ apps/react-client/src/components/cartProduct.tsx
@@ -62,78 +62,88 @@ const CartProduct = () => {
   return (
     cartContext.map((cart:any) => 
     
-    <Paper elevation={5}
+      
+      
+    <Paper elevation={3}
     key={cart._id}
       sx={{
         p: 2,
         margin: 'auto',
-        maxWidth: 500,
+        width:{xs:"100%",sm: 500},
         flexGrow: 1,
         marginBottom:"2rem",
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        borderRadius:"15px",
+        paddingLeft:"10px"
           
       }}
     >
-      <Grid container spacing={2} >
+      <Grid container spacing={1} sx={{position:"relative"}}>
         <Grid item>
         <NavLink to={`/products/${cart.item._id}`}>
-          <ButtonBase sx={{ width: 128, height: 128 }}>
+          <ButtonBase sx={{ width: {xs:128,sm:150}, height: {xs:128,sm:150} }}>
             <Img alt="complex" src={cart.item.image} />
           </ButtonBase>
           </NavLink>
         </Grid>
-        <Grid item xs={12} sm container>
+        <Grid item xs={7} sm container >
           <Grid item xs container direction="column" spacing={2}>
             <Grid item xs>
-                <NavLink to={`/products/${cart.item._id}`} style={{textDecoration:"none"}}>
-              <Typography gutterBottom variant="h6" component="div" color={"black"} sx={{fontWeight:"bold"}}>
-                {cart.item.title}
-              </Typography>
+              <NavLink to={`/products/${cart.item._id}`} style={{textDecoration:"none"}}>
+                <Typography gutterBottom variant="h6" component="div" color={"black"} sx={{fontWeight:"bold"}}>
+                  {cart.item.title}
+                </Typography>
               </NavLink>
-              {/* <Typography variant="body2" gutterBottom>
-              {cart.item.description}
-              </Typography> */}
+              
               <Typography variant="subtitle1" color="black" sx={{fontWeight:"bold"}}>
-              ₹{cart.item.currentPrice}
+                ₹{cart.item.currentPrice}
               </Typography>
             </Grid>
-            <Grid item>
+            <Grid item sx={{display:"flex",flexDirection:"row",justifyContent:"space-between",}}>
                 
-                <Typography  color={"black"} variant='body1' sx={{display:"flex",justifyContent:"start",alignItems:"center"}}>
+                <Typography  color={"black"} variant='body1' sx={{display:"flex",
+                  justifyContent:"space-between",alignItems:"center",
+                  fontWeight:"bold",border:"1px solid gray",
+                  width:"90px",borderRadius:"10px"}}
+                >
                     
                     <IconButton
                     onClick={()=>{updateCart(cart._id,cart.quantity-1)}}
                     disabled = {cart.quantity === 1 ? true:false}
+                    sx={{borderRight:"1px solid gray",borderRadius:"1px",width:"30px",color:"black"}}
                     >
-                        <RemoveIcon/>
+                        <RemoveIcon />
                     </IconButton>
 
                     {cart.quantity} 
 
                     <IconButton 
                     onClick={()=>{updateCart(cart._id,cart.quantity+1)}}
+                    disabled = {cart.quantity < 9 ? false:true}
+                    sx={{borderLeft:"1px solid gray",borderRadius:"1px",width:"30px",color:"black"}}
                     >
                         <AddIcon/>
                     </IconButton>
                 </Typography>
                 
             </Grid>
+            <Button variant='contained' color='error' size='small' startIcon={<DeleteIcon/>}
+              onClick={()=>{removeFromCart(cart._id)}}
+              sx={{position:"absolute",bottom:5,right:0}}
+            >
+              Remove
+            </Button> 
             
         </Grid>
-        <Grid item>
-            <Button variant='contained' color='error' size='small' startIcon={<DeleteIcon/>}
-            onClick={()=>{removeFromCart(cart._id)}}
-            >
-                Remove
-            </Button>
-        </Grid>
+        
           
         </Grid>
       </Grid>
       
     </Paper>
+      
+    
     )
+    
     
     
   );
