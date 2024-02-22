cd1f489798d009aaf6371acf4556b4e03de2bdf2
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 14:46:52 +0530

    chore: updated code formatting and organize imports

--- apps/react-client/src/components/requiresAuth.tsx
+++ apps/react-client/src/components/requiresAuth.tsx
@@ -1,21 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { useLoginStore } from "../contexts";
 
+export function RequiresAuth({ children }: { children: JSX.Element }) {
+  const loggedIn =
+    useLoginStore((state) => state.login) || localStorage.getItem("loggedIn");
 
-export function RequiresAuth( {children}:{children:JSX.Element}) {
-  const loggedIn = useLoginStore((state)=>state.login) || localStorage.getItem("loggedIn");
-
-  return (
-    <>
-    {loggedIn ? 
-        
-    children
-    
-   : 
-   
-    <Navigate to="/login" />
-    
-    }
-    </>
-  );
+  return <>{loggedIn ? children : <Navigate to="/login" />}</>;
 }
