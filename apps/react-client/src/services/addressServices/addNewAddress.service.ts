9e3e9c56af0948c1c0de7f26c17ab508f4b98ea5
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 12:42:30 +0530

    fix: corrected add Address Service function

--- apps/react-client/src/services/addressServices/addNewAddress.service.ts
+++ apps/react-client/src/services/addressServices/addNewAddress.service.ts
@@ -1,26 +1,20 @@
 import axios from "axios";
 import { addressType } from "common";
 
-const addAddressService = async(address:addressType)=>{
-
-
-    try{
-        return await axios.post(`/api/v1/address/newaddress`,{address},
-        {headers:{Authorization:`Bearer ${localStorage.getItem('token')}`}}
-        );
-
-
-    }catch(error){
-        if (axios.isAxiosError(error)) {
-            if (error && error.response) {
-              return error.response.data;
-            }
-        }
-
-        return {message:"Something Went Wrong"};
+const addAddressService = async (address: addressType) => {
+  try {
+    return await axios.post(`/api/v1/address/newaddress`, address, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error && error.response) {
+        return error.response.data;
+      }
     }
-    
 
-}
+    return { message: "Something Went Wrong" };
+  }
+};
 
 export default addAddressService;
