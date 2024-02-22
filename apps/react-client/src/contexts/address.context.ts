04f9d166937aab0edfb9b0cd67270ee136590ba2
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 12:35:25 +0530

    fix: adding in global address state

--- apps/react-client/src/contexts/address.context.ts
+++ apps/react-client/src/contexts/address.context.ts
@@ -1,24 +1,29 @@
-import {create } from "zustand";
+import { create } from "zustand";
 import { addressType } from "common";
 
-
-type addressState ={
-    address:addressType[];
-    setAddress: (allAddress:addressType[]) =>void;
-    addAdress:(newAddress:addressType)=>void;
-    updateAddress:(_id:any,updatedAddress:addressType)=>void;
-    removeAddress:(_id:any)=>void;
-}
-
+type addressState = {
+  address: addressType[];
+  setAddress: (allAddress: addressType[]) => void;
+  addAdress: (newAddress: addressType) => void;
+  updateAddress: (_id: any, updatedAddress: addressType) => void;
+  removeAddress: (_id: any) => void;
+};
 
 const useAddressStore = create<addressState>((set) => ({
-    address:[],
-    setAddress: (allAddress) => set(()=>({address:allAddress})),
-    addAdress: (newAddress) => set((state)=>({address:[...state.address,newAddress]})),
-    updateAddress: (_id,updatedAddress) => set((state)=>({address:state.address.map(address=>address._id===_id ? ({...address._id,updatedAddress}):address)})),
-    removeAddress:(_id) => set((state)=>({address:state.address.filter((address)=>address._id !== _id)}))
-    
-}))
+  address: [],
+  setAddress: (allAddress) => set(() => ({ address: allAddress })),
+  addAdress: (newAddress) =>
+    set((state) => ({ address: [...state.address, newAddress] })),
+  updateAddress: (_id, updatedAddress) =>
+    set((state) => ({
+      address: state.address.map((address) =>
+        address._id === _id ? updatedAddress : address
+      ),
+    })),
+  removeAddress: (_id) =>
+    set((state) => ({
+      address: state.address.filter((address) => address._id !== _id),
+    })),
+}));
 
 export default useAddressStore;
-
