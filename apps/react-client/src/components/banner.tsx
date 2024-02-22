7cce5f08596ccc70719ddb0f85a60fe5b3039fae
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 14:46:37 +0530

    chore: updated code formatting and organize imports

--- apps/react-client/src/components/banner.tsx
+++ apps/react-client/src/components/banner.tsx
@@ -1,72 +1,93 @@
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import useProductStore from '../contexts/productListing.context';
-
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import useProductStore from "../contexts/productListing.context";
 
 export default function Banner() {
-  const allProducts = useProductStore((state)=>state.allProducts);
-  const setInitialProducts = useProductStore((state)=>state.setInitialProducts)
-  const setProducts = useProductStore((state)=>state.setProducts);
+  const allProducts = useProductStore((state) => state.allProducts);
+  const setInitialProducts = useProductStore(
+    (state) => state.setInitialProducts
+  );
+  const setProducts = useProductStore((state) => state.setProducts);
   const navigate = useNavigate();
 
-    const post = {
-        title: 'Apna Fitness, Cause Your Fitness is in Your Hands',
-        description:
-          "Your One Stop Solution to All the Equipments, Supplements and Accessories At Best Price",
-          image:"https://t3.ftcdn.net/jpg/03/69/84/22/240_F_369842290_c1dQNWM8wnt3YD2uQTqupbda0XuksDXK.jpg",
-        imageText: 'Apna Fitness Banner Image',
-      };
-  
+  const post = {
+    title: "Apna Fitness, Cause Your Fitness is in Your Hands",
+    description:
+      "Your One Stop Solution to All the Equipments, Supplements and Accessories At Best Price",
+    image:
+      "https://t3.ftcdn.net/jpg/03/69/84/22/240_F_369842290_c1dQNWM8wnt3YD2uQTqupbda0XuksDXK.jpg",
+    imageText: "Apna Fitness Banner Image",
+  };
 
   return (
     <Paper
       sx={{
-        marginTop:{xs:"6.3rem",md:"auto"},
-        position: 'relative',
-        backgroundColor: 'grey.800',
-        color: '#fff',
+        marginTop: { xs: "6.3rem", md: "auto" },
+        position: "relative",
+        backgroundColor: "grey.800",
+        color: "#fff",
         mb: 4,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
         backgroundImage: `url(${post.image})`,
-        
       }}
     >
       {/* Increase the priority of the hero background image */}
-      {<img style={{ display: 'none', }} src={post.image} alt={post.imageText} />}
+      {
+        <img
+          style={{ display: "none" }}
+          src={post.image}
+          alt={post.imageText}
+        />
+      }
       <Box
         sx={{
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           bottom: 0,
           right: 0,
           left: 0,
-          backgroundColor: 'rgba(0,0,0,.3)',
+          backgroundColor: "rgba(0,0,0,.3)",
         }}
       />
       <Grid container>
         <Grid item md={6}>
           <Box
             sx={{
-              position: 'relative',
+              position: "relative",
               p: { xs: 3, md: 9 },
               pr: { md: 0 },
             }}
           >
-            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+            <Typography
+              component="h1"
+              variant="h3"
+              color="inherit"
+              gutterBottom
+            >
               {post.title}
             </Typography>
             <Typography variant="h5" color="inherit" paragraph>
               {post.description}
             </Typography>
-            
-              <Button variant='contained' color='primary' size='large' onClick={()=>{setInitialProducts(allProducts);setProducts(allProducts); navigate("/AllProducts")}}>SHOP NOW</Button>
-            
+
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={() => {
+                setInitialProducts(allProducts);
+                setProducts(allProducts);
+                navigate("/AllProducts");
+              }}
+            >
+              SHOP NOW
+            </Button>
           </Box>
         </Grid>
       </Grid>
