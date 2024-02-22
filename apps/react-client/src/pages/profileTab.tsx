ac61969dcc1f3c05ae767e8a7b55c1452b41a947
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 14:55:50 +0530

    chore: updated code formatting and organize imports

--- apps/react-client/src/pages/profileTab.tsx
+++ apps/react-client/src/pages/profileTab.tsx
@@ -1,25 +1,23 @@
-import Typography from '@mui/material/Typography';
-import { Box, Card, CardContent, styled } from '@mui/material';
-import useUsertStore from '../contexts/user.context';
-import LogOutButton from '../components/logOutButton';
-import PersonIcon from '@mui/icons-material/Person';
-import PhoneIcon from '@mui/icons-material/Phone';
-import EmailIcon from '@mui/icons-material/Email';
-
+import Typography from "@mui/material/Typography";
+import { Box, Card, CardContent, styled } from "@mui/material";
+import useUsertStore from "../contexts/user.context";
+import LogOutButton from "../components/logOutButton";
+import PersonIcon from "@mui/icons-material/Person";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 
 const StyledCard = styled(Card)(({ theme }) => ({
-    width: '350px',
-    [theme.breakpoints.up('sm')]: {
-      width: '480px', 
-    },
-    margin: 'auto',
-    marginTop:"4rem",
-    textAlign: 'center',
-    borderRadius: theme.spacing(2),
-    boxShadow: '0px 8px 20px rgba(0, 0, 0, 0.1)',
-    backgroundColor: theme.palette.background.paper,
-    border: `1px solid ${theme.palette.primary.main}`,
-    
+  width: "350px",
+  [theme.breakpoints.up("sm")]: {
+    width: "480px",
+  },
+  margin: "auto",
+  marginTop: "4rem",
+  textAlign: "center",
+  borderRadius: theme.spacing(2),
+  boxShadow: "0px 8px 20px rgba(0, 0, 0, 0.1)",
+  backgroundColor: theme.palette.background.paper,
+  border: `1px solid ${theme.palette.primary.main}`,
 }));
 
 const StyledCardContent = styled(CardContent)(({ theme }) => ({
@@ -28,75 +26,59 @@ const StyledCardContent = styled(CardContent)(({ theme }) => ({
 }));
 
 const ProfileItem = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
   marginBottom: theme.spacing(2),
-  
-  
 }));
 
-const IconWrapper = styled('span')(({ theme }) => ({
+const IconWrapper = styled("span")(({ theme }) => ({
   marginRight: theme.spacing(1),
   color: theme.palette.primary.main,
-  display:"flex",
-  
-  
+  display: "flex",
 }));
 
+const ProfileTab = () => {
+  const user = useUsertStore((state) => state.user);
 
-
-const ProfileTab = () =>{
-    const user = useUsertStore((state)=>state.user);
-
-    return (
-        <>
-        
-        <StyledCard>
+  return (
+    <>
+      <StyledCard>
         <StyledCardContent>
-            <PersonIcon sx={{ fontSize: 60, marginBottom: 1 }} />
-            <Typography variant="h5" gutterBottom>
+          <PersonIcon sx={{ fontSize: 60, marginBottom: 1 }} />
+          <Typography variant="h5" gutterBottom>
             User Profile
+          </Typography>
+
+          <ProfileItem>
+            <IconWrapper>
+              <PersonIcon />
+            </IconWrapper>
+            <Typography variant="h6">
+              {user.firstName} {user.lastName}
             </Typography>
-            
-            <ProfileItem>
-                <IconWrapper>
-                    <PersonIcon />
-                </IconWrapper>
-                <Typography variant="h6">
-                    {user.firstName} {user.lastName}
-                </Typography>
-            </ProfileItem>
+          </ProfileItem>
 
-            <ProfileItem>
-                <IconWrapper>
-                    <PhoneIcon />
-                </IconWrapper>
-                <Typography variant="h6">
-                    {user.mobile}
-                </Typography>
-            </ProfileItem>
+          <ProfileItem>
+            <IconWrapper>
+              <PhoneIcon />
+            </IconWrapper>
+            <Typography variant="h6">{user.mobile}</Typography>
+          </ProfileItem>
 
-            {user.email !== "" ?
-            
+          {user.email !== "" ? (
             <ProfileItem>
-                <IconWrapper>
-                    <EmailIcon />
-                </IconWrapper>
-                <Typography variant="h6">
-                    {user.email}
-                </Typography>
+              <IconWrapper>
+                <EmailIcon />
+              </IconWrapper>
+              <Typography variant="h6">{user.email}</Typography>
             </ProfileItem>
-
-            :
-                null
-            }
-            <LogOutButton/>
+          ) : null}
+          <LogOutButton />
         </StyledCardContent>
-    </StyledCard>
+      </StyledCard>
     </>
-
-    )
-}
+  );
+};
 
 export default ProfileTab;
