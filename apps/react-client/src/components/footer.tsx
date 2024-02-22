825609e7b4b86d2ce2f705ac4a311d3d6fb4d79c
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 14:42:50 +0530

    chore: updated code formatting and organize imports

--- apps/react-client/src/components/footer.tsx
+++ apps/react-client/src/components/footer.tsx
@@ -1,41 +1,38 @@
 import { Box, Container, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
-
-
-const Footer = ()=> {
-    return (
-        <Box
-          component="footer"
-          style={{marginTop:"auto",flex:"1"}}
-          sx={{
-            // position:{xs:"absolute",sm:"static"},
-            // bottom:"0vh",
-            
-            
-            width:"100%",
-            py: 3,
-            px: 2,
-            mt: 'auto',
-            mb:{xs:"3.5rem",sm:"auto"},
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-          }}
-        >
-        <Container maxWidth="sm" sx={{display:"flex",justifyContent:"center"}}>
+const Footer = () => {
+  return (
+    <Box
+      component="footer"
+      style={{ marginTop: "auto", flex: "1" }}
+      sx={{
+        width: "100%",
+        py: 3,
+        px: 2,
+        mt: "auto",
+        mb: { xs: "3.5rem", sm: "auto" },
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Container
+        maxWidth="sm"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
         <Typography variant="body2" color="text.secondary">
-            {'Copyright © '}
-            <Link color="inherit" to="/">
+          {"Copyright © "}
+          <Link color="inherit" to="/">
             ApnaFitness
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
         </Typography>
       </Container>
-      </Box>
-    );
-}
+    </Box>
+  );
+};
 
 export default Footer;
