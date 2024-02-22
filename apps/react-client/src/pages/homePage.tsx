52177b84dc60f7c93dad6d1e32538fa84a5ea0a5
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-22 14:51:32 +0530

    chore: updated code formatting and organize imports

--- apps/react-client/src/pages/homePage.tsx
+++ apps/react-client/src/pages/homePage.tsx
@@ -1,45 +1,58 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import { createTheme,ThemeProvider  } from '@mui/material/styles';
-import { Container, Toolbar } from '@mui/material';
-import Categories from '../components/category';
-import Banner from '../components/banner';
-import useProductStore from '../contexts/productListing.context';
-import Footer from '../components/footer';
-import HomeProductCard from '../components/homeProductCard';
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Container, Toolbar } from "@mui/material";
+import Categories from "../components/category";
+import Banner from "../components/banner";
+import useProductStore from "../contexts/productListing.context";
+import Footer from "../components/footer";
+import HomeProductCard from "../components/homeProductCard";
 
 const theme = createTheme();
 
+const HomePage = () => {
+  const allProducts = useProductStore((state) => state.allProducts);
 
-const HomePage = () =>  {
-  const allProducts = useProductStore((state)=>state.allProducts)
-
-  const products = allProducts.filter((product)=>product.isTrending);
-  
-
+  const products = allProducts.filter((product) => product.isTrending);
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-    <Container maxWidth="xl" sx={{position:"relative",marginTop:{md:"4.2rem"},width:"100%"}}>
-      
-      <Banner />
+      <CssBaseline />
+      <Container
+        maxWidth="xl"
+        sx={{
+          position: "relative",
+          marginTop: { md: "4.2rem" },
+          width: "100%",
+        }}
+      >
+        <Banner />
 
-      
-      <Typography color={"black"} variant='h4' textAlign={"center"} padding={"10px"}>Categories</Typography>
-      <Categories/>
+        <Typography
+          color={"black"}
+          variant="h4"
+          textAlign={"center"}
+          padding={"10px"}
+        >
+          Categories
+        </Typography>
+        <Categories />
 
-      <Toolbar/>
-      <Typography color={"black"} variant='h4' textAlign={"center"} padding={"10px"}>Our Products</Typography>
-      
-      <HomeProductCard products={products}/>
+        <Toolbar />
+        <Typography
+          color={"black"}
+          variant="h4"
+          textAlign={"center"}
+          padding={"10px"}
+        >
+          Our Products
+        </Typography>
 
-
-    </Container>
-    <Footer/>
+        <HomeProductCard products={products} />
+      </Container>
+      <Footer />
     </ThemeProvider>
-  )
-}
-
+  );
+};
 
 export default HomePage;
