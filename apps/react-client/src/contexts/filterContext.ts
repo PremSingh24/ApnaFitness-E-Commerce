8d79423579a1e22f63c95413e94d2e2a0b269338
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-23 23:39:01 +0530

    chore: removed extra context state

--- apps/react-client/src/contexts/filterContext.ts
+++ apps/react-client/src/contexts/filterContext.ts
@@ -1,35 +1,46 @@
-import {create } from "zustand";
+import { create } from "zustand";
 
-
+type allFilterStateType = {
+  priceRange: number[];
+  rating: number;
+  removeOutOfStock: boolean;
+  fastDeliveryOnly: boolean;
+};
 
 type filterState = {
-    sortBy:string,
-    setSortBy:(sortBy:string) =>void;
-    priceRange:number[],
-    setPriceRange:(priceRange:number[]) =>void;
-    rating:number,
-    setRating:(rating:number) =>void;
-    removeOutOfStock:boolean,
-    setRemoveOutOfStock:(removeOutOfStock:boolean) =>void;
-    fastDeliveryOnly:boolean,
-    setFastDeliveryOnly:(fastDeliveryOnly:boolean) =>void;
-}
-
+  sortBy: string;
+  setSortBy: (sortBy: string) => void;
+  priceRange: number[];
+  setPriceRange: (priceRange: number[]) => void;
+  rating: number;
+  setRating: (rating: number) => void;
+  removeOutOfStock: boolean;
+  setRemoveOutOfStock: (removeOutOfStock: boolean) => void;
+  fastDeliveryOnly: boolean;
+  setFastDeliveryOnly: (fastDeliveryOnly: boolean) => void;
+  setAllFilters: (allFilterState: allFilterStateType) => void;
+};
 
 const useFilterStore = create<filterState>((set) => ({
-    sortBy:"",
-    setSortBy: (sortBy) => set(()=>({sortBy:sortBy})),
-    priceRange:[0,10000],
-    setPriceRange:(priceRange) => set(()=>({priceRange:priceRange})),
-    rating:1,
-    setRating:(rating) => set(()=>({rating:rating})),
-    removeOutOfStock:false,
-    setRemoveOutOfStock:(removeOutOfStock) =>set(()=>({removeOutOfStock:removeOutOfStock})),
-    fastDeliveryOnly:false,
-    setFastDeliveryOnly:(fastDeliveryOnly) => set(()=>({fastDeliveryOnly:fastDeliveryOnly})),
-    
-    
-}))
+  sortBy: "",
+  setSortBy: (sortBy) => set(() => ({ sortBy: sortBy })),
+  priceRange: [0, 10000],
+  setPriceRange: (priceRange) => set(() => ({ priceRange: priceRange })),
+  rating: 1,
+  setRating: (rating) => set(() => ({ rating: rating })),
+  removeOutOfStock: false,
+  setRemoveOutOfStock: (removeOutOfStock) =>
+    set(() => ({ removeOutOfStock: removeOutOfStock })),
+  fastDeliveryOnly: false,
+  setFastDeliveryOnly: (fastDeliveryOnly) =>
+    set(() => ({ fastDeliveryOnly: fastDeliveryOnly })),
+  setAllFilters: (allFilterState) =>
+    set(() => ({
+      priceRange: allFilterState.priceRange,
+      rating: allFilterState.rating,
+      removeOutOfStock: allFilterState.removeOutOfStock,
+      fastDeliveryOnly: allFilterState.fastDeliveryOnly,
+    })),
+}));
 
 export default useFilterStore;
-
