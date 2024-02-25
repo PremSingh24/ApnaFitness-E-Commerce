5e8f01813668f0ddb73d5757bcf3789bd2351cac
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-25 21:30:17 +0530

    chore: reset user data on logout

--- apps/react-client/src/components/logOutButton.tsx
+++ apps/react-client/src/components/logOutButton.tsx
@@ -2,37 +2,40 @@ import { Button } from "@mui/material";
 import { useLoginStore } from "../contexts";
 import { NavLink } from "react-router-dom";
 import { toast } from "sonner";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import useWishlistStore from "../contexts/wishlist.context";
+import useCartStore from "../contexts/cart.context";
 
+const LogOutButton = () => {
+  const setLogOut = useLoginStore((state) => state.setLogOut);
+  const setWishlist = useWishlistStore((state) => state.setWishlist);
+  const setCart = useCartStore((state) => state.setCart);
 
+  const logOut = () => {
+    localStorage.setItem("token", "");
+    localStorage.removeItem("loggedIn");
+    setCart([]);
+    setWishlist([]);
+    setLogOut();
+    toast.error("You Logged Out!!");
+  };
 
+  return (
+    <NavLink to={"/"} style={{ width: "50%" }}>
+      <Button
+        variant="contained"
+        color="error"
+        startIcon={<ExitToAppIcon />}
+        size="large"
+        onClick={() => {
+          logOut();
+        }}
+        sx={{ marginTop: "20px", borderRadius: "20px", width: "60%" }}
+      >
+        LOG OUT
+      </Button>
+    </NavLink>
+  );
+};
 
-
-const LogOutButton = ()=> {
-    const setLogOut = useLoginStore((state)=>state.setLogOut)
-   
-
-    const logOut = ()=> {
-        localStorage.setItem("token","")
-        localStorage.removeItem("loggedIn")
-        setLogOut();
-        toast.error("You Logged Out!!")
-
-    } 
-
-    return (
-        <NavLink to={"/"} style={{width:"50%"}}>
-            <Button variant="contained" color="error" 
-                startIcon={<ExitToAppIcon/>}
-                size="large"
-                onClick={()=>{logOut()}}
-                sx={{marginTop:"20px",borderRadius:"20px",width:"60%"}}
-            >
-                LOG OUT
-            </Button>
-        </NavLink>
-    )
-
-}
-
-export default LogOutButton
+export default LogOutButton;
