57abd55be96e2a1e6ce075d41e8edd6b57330ec5
Prem Singh <PremSingh.Official29@gmail.com>
2024-02-25 21:31:19 +0530

    chore: added wishlist badge

--- apps/react-client/src/components/phoneBottomNavigation.tsx
+++ apps/react-client/src/components/phoneBottomNavigation.tsx
@@ -1,13 +1,16 @@
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Badge, BottomNavigation, BottomNavigationAction } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import PersonIcon from "@mui/icons-material/Person";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useWishlistStore from "../contexts/wishlist.context";
 
 const PhoneBottomNavigation = () => {
   const [value, setValue] = useState(window.location.pathname);
   const navigate = useNavigate();
+
+  const wishlist = useWishlistStore((state) => state.wishlist);
 
   return (
     <BottomNavigation
@@ -35,9 +38,18 @@ const PhoneBottomNavigation = () => {
         }}
         value={"/"}
       />
+
       <BottomNavigationAction
         label="wishlist"
-        icon={<FavoriteIcon />}
+        icon={
+          <Badge
+            badgeContent={wishlist.length > 0 ? wishlist.length : null}
+            max={9}
+            color="error"
+          >
+            <FavoriteIcon />
+          </Badge>
+        }
         onClick={() => {
           navigate("/mywishlist");
         }}
