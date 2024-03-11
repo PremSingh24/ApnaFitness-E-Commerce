134eb327e012f0fc96f3dfbd90ccb4068b6a2fe0
Prem Singh <PremSingh.Official29@gmail.com>
2024-03-11 21:10:03 +0530

    feat: enhance 'order' schema for improved structure and clarity

--- apps/server/src/models/order.model.ts
+++ apps/server/src/models/order.model.ts
@@ -2,52 +2,59 @@ import mongoose from "mongoose";
 import { AddressSchema } from "./user.model";
 
 const OrderSchema = new mongoose.Schema(
-    {
-        status:{
-            type:String,
-            enum:["Order Confirmed","Shipped","Out For Delivery","Delivered"],
-            default:"Order Confirmed"
-        },
-        orderedBy:{
-            name:String,
-            mobile:String
+  {
+    status: {
+      type: String,
+      enum: ["Order Confirmed", "Shipped", "Out For Delivery", "Delivered"],
+      default: "Order Confirmed",
+    },
+    orderedBy: {
+      name: String,
+      mobile: String,
+    },
+    product: {
+      name: {
+        type: String,
+        required: true,
+      },
+      image: {
+        type: String,
+        require: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
 
-        },
-        item:{   
-            type:String,
-            required:true,
-        },
-        image:{
-            type:String,
-            require:true
-        },
-        quantity:{
-            type:Number,
-            default:1
-        },
-        price:{
-            type:Number,
-            required:true
-        },
-        deliveryAddress:{
-            type:AddressSchema
-        },
-        deliveryCharge:{
-            type:Number,
-            default:45
-        },
-        discount:{
-            type:Number,
-            default:0
-        },
-        totalAmount:{
-            type:Number,
-            required:true
-        },
-        
+    deliveryAddress: {
+      type: AddressSchema,
+      required: true,
+    },
+    priceDetails: {
+      deliveryCharge: {
+        type: Number,
+        default: 45,
+      },
+      discount: {
+        type: Number,
+        default: 0,
+      },
+      totalAmount: {
+        type: Number,
+        required: true,
+      },
+    },
+    paymentId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
-    },{timestamps:true}
-)
-
-export const Orders = mongoose.model("Orders",OrderSchema);
+export const Orders = mongoose.model("Orders", OrderSchema);
