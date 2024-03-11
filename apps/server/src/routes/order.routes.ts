819b939116f99cc78d276de30eb3be8fc5fda3ef
Prem Singh <PremSingh.Official29@gmail.com>
2024-03-11 21:05:02 +0530

    feat: add routes for 'verifyOrder' and 'verifyPayment' services

--- apps/server/src/routes/order.routes.ts
+++ apps/server/src/routes/order.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticateJwt } from "../middleware/auth";
-import { getOrderItemsHandler, placeOrderHandler } from "../controllers/order.controller";
+import {
+  getOrderItemsHandler,
+  verifyOrderHandler,
+  verifyPaymentHandler,
+} from "../controllers/order.controller";
 
 const router = Router();
 
-router.route("/myorders").get(authenticateJwt , getOrderItemsHandler);
-router.route("/placeorder").post(authenticateJwt , placeOrderHandler);
-
+router.route("/myorders").get(authenticateJwt, getOrderItemsHandler);
+router.route("/verifyorder").post(authenticateJwt, verifyOrderHandler);
+router.route("/verifypayment").post(authenticateJwt, verifyPaymentHandler);
 
 export default router;
