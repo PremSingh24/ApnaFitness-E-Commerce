c2f1875bec6b532573791bee7d0a3718f63f08fa
Prem Singh <PremSingh.Official29@gmail.com>
2024-03-30 23:21:45 +0530

    chore: removed spaces

--- apps/react-client/src/services/authServices/login.service.ts
+++ apps/react-client/src/services/authServices/login.service.ts
@@ -1,24 +1,18 @@
 import axios from "axios";
-import {loginUserType} from "common"
+import { loginUserType } from "common";
 
-const loginService = async(registerUser:loginUserType)=>{
-
-    try{
-        return await axios.post("/api/v1/auth/login", registerUser);
-
-         
-
-    }catch(error){
-
-        if (axios.isAxiosError(error)) {
-            if (error && error.response) {
-                return error.response.data;
-            }
-        }
-
-        return { message: "Something Went Wrong!" };
+const loginService = async (loginUser: loginUserType) => {
+  try {
+    return await axios.post("/api/v1/auth/login", loginUser);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error && error.response) {
+        return error.response.data;
+      }
     }
 
-}
+    return { message: "Something Went Wrong!" };
+  }
+};
 
 export default loginService;
