612393061d4f7fb1a7b9ac116d0c921edb68e4bf
PremSingh24 <premsingh.official29@gmail.com>
2024-05-28 02:33:54 +0530

    feat: add route for trending products

--- apps/server/src/routes/product.routes.ts
+++ apps/server/src/routes/product.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { getAllProductsHandler, getProductHandler } from "../controllers/product.controller";
+import {
+  getAllProductsHandler,
+  getProductHandler,
+  getTrendingProductsHandler,
+} from "../controllers/product.controller";
 
 const router = Router();
+
+router.route("/trending").get(getTrendingProductsHandler);
 
 router.route("/all").get(getAllProductsHandler);
 
 router.route("/:ProductId").get(getProductHandler);
-
-//router.route("/add").post(newProduct)
-
 
 export default router;
