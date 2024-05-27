77dca6aa73963bb4dbe32060d9e33a1f730fb43e
PremSingh24 <premsingh.official29@gmail.com>
2024-05-27 23:08:27 +0530

    chore: add productId type

--- apps/react-client/src/services/productServices/getProduct.service.ts
+++ apps/react-client/src/services/productServices/getProduct.service.ts
@@ -1,24 +1,17 @@
 import axios from "axios";
 
-
-const getProductService = async(ProductId:any)=>{
-
-    try{
-        return await axios.get(`/api/v1/products/${ProductId}`);
-
-          
-    }catch(error){
-
-        if (axios.isAxiosError(error)) {
-            if (error && error.response) {
-              return error.response.data;
-            }
-        }
-      
-        return { message: "Something went wrong!" };
+const getProductService = async (ProductId: string) => {
+  try {
+    return await axios.get(`/api/v1/products/${ProductId}`);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error && error.response) {
+        return error.response.data;
+      }
     }
-    
 
-}
+    return { message: "Something went wrong!" };
+  }
+};
 
 export default getProductService;
