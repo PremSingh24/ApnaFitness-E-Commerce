0c2ab842d917e80f604777e307ef3e84c09af800
PremSingh24 <premsingh.official29@gmail.com>
2024-05-31 01:43:38 +0530

    refactor: change product storing in state

--- apps/react-client/src/hooks/useProductFetch.ts
+++ apps/react-client/src/hooks/useProductFetch.ts
@@ -1,40 +1,28 @@
 "use client";
 import { useEffect } from "react";
 import useProductStore from "../store/productListing.store";
-import useCategoryStore from "../store/category.store";
 import getAllProductService from "../services/productServices/getAllProducts.service";
-import getAllCategoriesService from "../services/categoryServices/getAllCategories.service";
 import { toast } from "sonner";
 
 const useFetchAllProducts = () => {
+  const allProducts = useProductStore((state) => state.allProducts);
   const setAllProducts = useProductStore((state) => state.setAllProducts);
-  const setProducts = useProductStore((state) => state.setProducts);
-
-  const setCategories = useCategoryStore((state) => state.setCategories);
 
   useEffect(() => {
     (async () => {
-      //Getting All Products
-      const productResponse = await getAllProductService();
+      if (allProducts.length === 0) {
+        //Getting All Products
+        const productResponse = await getAllProductService();
 
-      if (productResponse.status === 200) {
-        setAllProducts(productResponse.data.products);
-        setProducts(productResponse.data.products);
-      } else {
-        toast.error(
-          productResponse?.message
-            ? productResponse.message
-            : productResponse.data.message
-        );
-      }
-
-      //Getting ALl Categories
-      const categoryResponse = await getAllCategoriesService();
-
-      if (categoryResponse.status === 200) {
-        setCategories(categoryResponse.data.category);
-      } else {
-        toast.error("Something Went Wrong");
+        if (productResponse.status === 200) {
+          setAllProducts(productResponse.data.products);
+        } else {
+          toast.error(
+            productResponse?.message
+              ? productResponse.message
+              : productResponse.data.message
+          );
+        }
       }
     })();
   }, []);
