31b77fe201849eed05ae4a39d2b70bbd0452569b
PremSingh24 <premsingh.official29@gmail.com>
2024-05-31 02:56:29 +0530

    feat: add route rewrites and redirects, and configure new image domain

--- apps/react-client/next.config.js
+++ apps/react-client/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   // output: "export", // Outputs a Single-Page Application (SPA).
   // distDir: "./dist", // Changes the build output directory to `./dist/`.
+  images: {
+    domains: ["t3.ftcdn.net"],
+  },
 
   async rewrites() {
     return [
@@ -10,10 +13,32 @@ const nextConfig = {
         destination: "http://localhost:3000/api/:path*", // Proxy to Backend
       },
       {
-        source: "/AllProducts",
-        destination: "/productListingPage",
+        source: "/home",
+        destination: "/",
+      },
+      {
+        source: "/homepage",
+        destination: "/",
+      },
+      {
+        source: "/allProducts",
+        destination: "/products",
       },
     ];
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/user",
+        destination: "/user/profile",
+        permanent: true,
+      },
+    ];
+  },
+
+  images: {
+    domains: ["res.cloudinary.com"],
   },
 };
 
