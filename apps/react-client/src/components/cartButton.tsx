0bc57402b335610f107dc445edb1a24f2c81a9ff
PremSingh24 <premsingh.official29@gmail.com>
2024-05-31 01:50:14 +0530

    refactor: extract CartButton into a separate component

---
+++ apps/react-client/src/components/cartButton.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import useCartStore from "../store/cart.store";
+import addToCartService from "../services/cartServices/addToCart.service";
+import useLoginStore from "../store/login.store";
+import useLogOut from "../hooks/useLogOut";
+import { productType } from "common";
+
+const CartButton = ({ product }: { product: productType }) => {
+  const cart = useCartStore((state) => state.cart);
+  const addToCartContext = useCartStore((state) => state.addToCart);
+  const router = useRouter();
+
+  const loggedIn =
+    useLoginStore((state) => state.login) ||
+    document.cookie === "loggedIn=true";
+
+  const logOut = useLogOut();
+
+  const addToCart = async (product: any, ProductId: any) => {
+    const response = await addToCartService(ProductId);
+    if (response.status === 201) {
+      addToCartContext(product, response.data.cartId);
+      toast.success(response.data.message);
+    } else if (response.status === 401) {
+      await logOut();
+      toast.error("Session Expired, Login Again!");
+    } else {
+      toast.error(response.message ? response.message : response.data.message);
+    }
+  };
+
+  return (
+    <>
+      {product.inStock ? (
+        cart.find((obj) => obj.item._id === product._id) ? (
+          <Button
+            variant="outlined"
+            size="medium"
+            fullWidth
+            onClick={() =>
+              loggedIn ? router.push("/cart") : router.push("/login")
+            }
+          >
+            Go To Cart
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            size="medium"
+            fullWidth
+            onClick={() =>
+              loggedIn ? addToCart(product, product._id) : router.push("/login")
+            }
+          >
+            Add To Cart
+          </Button>
+        )
+      ) : (
+        <Button
+          variant="contained"
+          size="medium"
+          color="error"
+          fullWidth
+          disabled
+        >
+          Out Of Stock
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default CartButton;
