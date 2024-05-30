0ba675fa68b0ee3f4c282c19b98dda610b175438
PremSingh24 <premsingh.official29@gmail.com>
2024-05-31 01:43:56 +0530

    refactor: remove extra store variable

--- apps/react-client/src/store/productListing.store.ts
+++ apps/react-client/src/store/productListing.store.ts
@@ -3,16 +3,12 @@ import { productType } from "common";
 
 type productsState = {
   allProducts: productType[];
-  products: productType[];
   setAllProducts: (products: productType[]) => void;
-  setProducts: (products: productType[]) => void;
 };
 
 const useProductStore = create<productsState>((set) => ({
   allProducts: [],
-  products: [],
   setAllProducts: (products) => set(() => ({ allProducts: products })),
-  setProducts: (products) => set(() => ({ products: products })),
 }));
 
 export default useProductStore;
