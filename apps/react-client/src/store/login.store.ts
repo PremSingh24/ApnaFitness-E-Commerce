f76ccb9bb7a2d0fbf17a27800febbef64aee9f2d
PremSingh24 <premsingh.official29@gmail.com>
2024-06-06 01:04:44 +0530

    chore: remove space

--- apps/react-client/src/store/login.store.ts
+++ apps/react-client/src/store/login.store.ts
@@ -1,20 +1,15 @@
-import {create } from "zustand";
+import { create } from "zustand";
 
-
-
-type LoginState ={
-    login:boolean;
-    setLogin:()=>void;
-    setLogOut:()=>void;
-}
-
+type LoginState = {
+  login: boolean;
+  setLogin: () => void;
+  setLogOut: () => void;
+};
 
 const useLoginStore = create<LoginState>((set) => ({
-    login: false,
-    setLogin: () => set({ login: true}),
-    setLogOut: ()=> set(()=>({login:false})),
-    
-}))
+  login: false,
+  setLogin: () => set({ login: true }),
+  setLogOut: () => set(() => ({ login: false })),
+}));
 
 export default useLoginStore;
-
