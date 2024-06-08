0ae4ec9f92c63a83fc4ffce401630d270bf04670
PremSingh24 <premsingh.official29@gmail.com>
2024-06-08 10:04:03 +0530

    feat: implement loader for login transition

--- apps/Next-client/src/app/(auth)/login/page.tsx
+++ apps/Next-client/src/app/(auth)/login/page.tsx
@@ -16,13 +16,16 @@ import { useRouter } from "next/navigation";
 import useLoginStore from "../../../store/login.store";
 import PasswordField from "../../../components/passwordField";
 import loginUserService from "../../../services/authServices/login.service";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loader from "../../../components/loader";
 
 const defaultTheme = createTheme();
 
 const LoginPage = () => {
   const router = useRouter();
   const setLogin = useLoginStore((state) => state.setLogin);
+
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const loggedIn = document.cookie === "loggedIn=true";
@@ -45,6 +48,7 @@ const LoginPage = () => {
   const { errors } = formState;
 
   const loginUser = async (data: loginUserType) => {
+    setLoading(true);
     const response = await loginUserService(data);
 
     if (response.status === 200) {
@@ -58,6 +62,7 @@ const LoginPage = () => {
 
       toast.success(response.data.message);
     } else {
+      setLoading(false);
       toast.error(response.message ? response.message : response.data.message);
     }
   };
@@ -74,114 +79,118 @@ const LoginPage = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Toolbar />
-      <Container
-        component="main"
-        maxWidth="xs"
-        sx={{
-          height: { xs: "80vh", sm: "100%" },
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <CssBaseline />
-        <Paper
-          variant="elevation"
-          elevation={5}
+      {!loading ? (
+        <Container
+          component="main"
+          maxWidth="xs"
           sx={{
-            marginTop: 1,
+            height: { xs: "80vh", sm: "100%" },
             display: "flex",
-            flexDirection: "column",
+            justifyContent: "center",
             alignItems: "center",
-            my: { xs: 3, md: 9 },
-            p: { xs: 3, md: 5 },
-            borderRadius: "5%",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log In
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit(loginUser)}
-            sx={{ mt: 3 }}
+          <CssBaseline />
+          <Paper
+            variant="elevation"
+            elevation={5}
+            sx={{
+              marginTop: 1,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              my: { xs: 3, md: 9 },
+              p: { xs: 3, md: 5 },
+              borderRadius: "5%",
+            }}
           >
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <TextField
-                  placeholder="Mobile Number/Email Id"
-                  id="mobile"
-                  type="phone"
-                  fullWidth
-                  required
-                  {...register("email", {
-                    required: "Mobile Number/ Email Id Required",
-                    minLength: {
-                      value: 10,
-                      message: "Invalid Mobile Number/Email Id",
-                    },
-                    pattern: {
-                      value: /^\S*$/,
-                      message: "Space Detected",
-                    },
-                  })}
-                  error={!!errors.email}
-                  helperText={errors.email?.message}
-                  InputProps={{ inputProps: { maxLength: 30 } }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <PasswordField
-                  placeholder="Password"
-                  id="password"
-                  register={register}
-                  errors={!!errors.password}
-                  helperText={errors.password?.message}
-                  message="Password is Required"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              size="large"
-              variant="contained"
-              sx={{ mt: 4, mb: 2, borderRadius: 20, fontWeight: "bold" }}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log In
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(loginUser)}
+              sx={{ mt: 3 }}
             >
-              Login
-            </Button>
-            <Button
-              type="button"
-              fullWidth
-              color="secondary"
-              size="large"
-              variant="contained"
-              sx={{
-                backgroundColor: "rgb(147 51 234)",
-                borderRadius: 20,
-                textTransform: "none",
-                fontWeight: "bold",
-                letterSpacing: "0.5px",
-                mb: 3,
-              }}
-              onClick={() => {
-                addGuestCredential();
-              }}
-            >
-              Use Guest Credentials
-            </Button>
-            <Grid container justifyContent="space-between">
-              <Grid item>
-                <Link href="/register">New User? Sign Up</Link>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField
+                    placeholder="Mobile Number/Email Id"
+                    id="mobile"
+                    type="phone"
+                    fullWidth
+                    required
+                    {...register("email", {
+                      required: "Mobile Number/ Email Id Required",
+                      minLength: {
+                        value: 10,
+                        message: "Invalid Mobile Number/Email Id",
+                      },
+                      pattern: {
+                        value: /^\S*$/,
+                        message: "Space Detected",
+                      },
+                    })}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                    InputProps={{ inputProps: { maxLength: 30 } }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <PasswordField
+                    placeholder="Password"
+                    id="password"
+                    register={register}
+                    errors={!!errors.password}
+                    helperText={errors.password?.message}
+                    message="Password is Required"
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        </Paper>
-      </Container>
+              <Button
+                type="submit"
+                fullWidth
+                size="large"
+                variant="contained"
+                sx={{ mt: 4, mb: 2, borderRadius: 20, fontWeight: "bold" }}
+              >
+                Login
+              </Button>
+              <Button
+                type="button"
+                fullWidth
+                color="secondary"
+                size="large"
+                variant="contained"
+                sx={{
+                  backgroundColor: "rgb(147 51 234)",
+                  borderRadius: 20,
+                  textTransform: "none",
+                  fontWeight: "bold",
+                  letterSpacing: "0.5px",
+                  mb: 3,
+                }}
+                onClick={() => {
+                  addGuestCredential();
+                }}
+              >
+                Use Guest Credentials
+              </Button>
+              <Grid container justifyContent="space-between">
+                <Grid item>
+                  <Link href="/register">New User? Sign Up</Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+        </Container>
+      ) : (
+        <Loader />
+      )}
     </ThemeProvider>
   );
 };
--- apps/Next-client/src/app/(auth)/register/page.tsx
+++ apps/Next-client/src/app/(auth)/register/page.tsx
@@ -16,13 +16,14 @@ import { useRouter } from "next/navigation";
 import useLoginStore from "../../../store/login.store";
 import { registerUserService } from "../../../services/authServices/register.service";
 import PasswordField from "../../../components/passwordField";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loader from "../../../components/loader";
 
 const defaultTheme = createTheme();
 
 const RegisterPage = () => {
   const router = useRouter();
-  const setLogin = useLoginStore((state) => state.setLogin);
+
   useEffect(() => {
     const loggedIn = document.cookie === "loggedIn=true";
 
@@ -31,6 +32,10 @@ const RegisterPage = () => {
       return;
     }
   }, []);
+
+  const setLogin = useLoginStore((state) => state.setLogin);
+
+  const [loading, setLoading] = useState<boolean>(false);
 
   const userData = useForm<registerUserType>({
     defaultValues: {
@@ -52,6 +57,7 @@ const RegisterPage = () => {
     } else {
       let response;
       if (data.email === "") {
+        setLoading(true);
         const { firstName, lastName, mobile, password } = data;
 
         response = await registerUserService({
@@ -61,6 +67,7 @@ const RegisterPage = () => {
           password,
         });
       } else {
+        setLoading(true);
         response = await registerUserService(data);
       }
 
@@ -74,6 +81,7 @@ const RegisterPage = () => {
         router.replace("/");
         toast.success(response.data.message);
       } else {
+        setLoading(false);
         toast.error(
           response.message ? response.message : response.data.message
         );
@@ -83,157 +91,162 @@ const RegisterPage = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container
-        component="main"
-        maxWidth="xs"
-        sx={{ marginTop: { xs: "8rem", md: "6rem" } }}
-      >
-        <CssBaseline />
-        <Paper
-          variant="elevation"
-          elevation={24}
-          sx={{
-            marginTop: 1,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            my: { xs: 3, md: 6 },
-            p: { xs: 3, md: 4 },
-            borderRadius: "7%",
-          }}
+      {!loading ? (
+        <Container
+          component="main"
+          maxWidth="xs"
+          sx={{ marginTop: { xs: "8rem", md: "6rem" } }}
         >
-          <Avatar sx={{ m: 0, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit(registerUser)}
-            sx={{ mt: 2 }}
+          <CssBaseline />
+          <Paper
+            variant="elevation"
+            elevation={24}
+            sx={{
+              marginTop: 1,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              my: { xs: 3, md: 6 },
+              p: { xs: 3, md: 4 },
+              borderRadius: "7%",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="First Name"
-                  id="firstName"
-                  type="text"
-                  fullWidth
-                  required
-                  {...register("firstName", {
-                    required: "First Name is Required",
-                    minLength: { value: 2, message: "Minimum 2 Characters" },
-                    pattern: {
-                      value: /^\S*$/,
-                      message: "Space Detected",
-                    },
-                  })}
-                  error={!!errors.firstName}
-                  helperText={errors.firstName?.message}
-                  InputProps={{ inputProps: { maxLength: 25 } }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Last Name"
-                  id="lastName"
-                  type="text"
-                  fullWidth
-                  required
-                  {...register("lastName", {
-                    required: "Last Name is Required",
-                    minLength: { value: 2, message: "Minimum 2 Characters" },
-                    pattern: {
-                      value: /^\S*$/,
-                      message: "Space Detected",
-                    },
-                  })}
-                  error={!!errors.lastName}
-                  helperText={errors.lastName?.message}
-                  InputProps={{ inputProps: { maxLength: 25 } }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Mobile Number"
-                  prefix="+91"
-                  id="mobile"
-                  fullWidth
-                  required
-                  {...register("mobile", {
-                    required: "Mobile Number is Required",
-                    minLength: {
-                      value: 10,
-                      message: "There are less than 10 digits",
-                    },
-                    pattern: {
-                      value:
-                        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-                      message: "Please Enter a Valid Mobile Number",
-                    },
-                  })}
-                  error={!!errors.mobile}
-                  helperText={errors.mobile?.message}
-                  InputProps={{ inputProps: { maxLength: 10 } }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="Email (Optional)"
-                  id="email"
-                  placeholder="AmanGupta@gmail.com"
-                  type="email"
-                  fullWidth
-                  {...register("email", {
-                    pattern: {
-                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                      message: "Invalid email address",
-                    },
-                  })}
-                  error={!!errors.email}
-                  helperText={errors.email?.message}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <PasswordField
-                  placeholder="Password"
-                  id="password"
-                  register={register}
-                  errors={!!errors.password}
-                  helperText={errors.password?.message}
-                  message="Password is Required"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <PasswordField
-                  placeholder="Confirm Password"
-                  id="confirmPassword"
-                  register={register}
-                  errors={!!errors.confirmPassword}
-                  helperText={errors.confirmPassword?.message}
-                  message="Confirm Your Password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              size="large"
-              variant="contained"
-              sx={{ mt: 3, mb: 2, borderRadius: 5 }}
-            >
+            <Avatar sx={{ m: 0, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
               Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login">Already a User? Log in</Link>
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(registerUser)}
+              sx={{ mt: 2 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="First Name"
+                    id="firstName"
+                    type="text"
+                    fullWidth
+                    required
+                    {...register("firstName", {
+                      required: "First Name is Required",
+                      minLength: { value: 2, message: "Minimum 2 Characters" },
+                      pattern: {
+                        value: /^\S*$/,
+                        message: "Space Detected",
+                      },
+                    })}
+                    error={!!errors.firstName}
+                    helperText={errors.firstName?.message}
+                    InputProps={{ inputProps: { maxLength: 25 } }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label="Last Name"
+                    id="lastName"
+                    type="text"
+                    fullWidth
+                    required
+                    {...register("lastName", {
+                      required: "Last Name is Required",
+                      minLength: { value: 2, message: "Minimum 2 Characters" },
+                      pattern: {
+                        value: /^\S*$/,
+                        message: "Space Detected",
+                      },
+                    })}
+                    error={!!errors.lastName}
+                    helperText={errors.lastName?.message}
+                    InputProps={{ inputProps: { maxLength: 25 } }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Mobile Number"
+                    prefix="+91"
+                    id="mobile"
+                    fullWidth
+                    required
+                    {...register("mobile", {
+                      required: "Mobile Number is Required",
+                      minLength: {
+                        value: 10,
+                        message: "There are less than 10 digits",
+                      },
+                      pattern: {
+                        value:
+                          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+                        message: "Please Enter a Valid Mobile Number",
+                      },
+                    })}
+                    error={!!errors.mobile}
+                    helperText={errors.mobile?.message}
+                    InputProps={{ inputProps: { maxLength: 10 } }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label="Email (Optional)"
+                    id="email"
+                    placeholder="AmanGupta@gmail.com"
+                    type="email"
+                    fullWidth
+                    {...register("email", {
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: "Invalid email address",
+                      },
+                    })}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <PasswordField
+                    placeholder="Password"
+                    id="password"
+                    register={register}
+                    errors={!!errors.password}
+                    helperText={errors.password?.message}
+                    message="Password is Required"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <PasswordField
+                    placeholder="Confirm Password"
+                    id="confirmPassword"
+                    register={register}
+                    errors={!!errors.confirmPassword}
+                    helperText={errors.confirmPassword?.message}
+                    message="Confirm Your Password"
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
-        </Paper>
-      </Container>
+              <Button
+                type="submit"
+                fullWidth
+                size="large"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, borderRadius: 5 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/login">Already a User? Log in</Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+        </Container>
+      ) : (
+        <Loader />
+      )}
     </ThemeProvider>
   );
 };
